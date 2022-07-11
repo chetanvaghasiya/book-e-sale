5d739dff133061a21e980a4be008329f1934e96a
chetanvaghasiya <chetanvaghasiya2001@gmail.com>
2022-07-11 14:37:25 +0530

    first commit

---
+++ BookStore (2)/BookStore (2)/BookStore/BookStore.API/BookStore.Repositories/Migrations/20220516095019_Initial.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Repositories.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "category",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false, defaultValueSql: "nextval('categories_id_seq'::regclass)"),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_category", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "publisher",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false, defaultValueSql: "nextval('publisher_id_seq'::regclass)"),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    address = table.Column<string>(type: "text", nullable: true),
+                    contact = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_publisher", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "role",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false, defaultValueSql: "nextval('roles_id_seq'::regclass)"),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "book",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false, defaultValueSql: "nextval('books_id_seq'::regclass)"),
+                    name = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    price = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true),
+                    base64image = table.Column<string>(type: "text", nullable: true),
+                    categoryid = table.Column<int>(type: "integer", nullable: false),
+                    publisherid = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_book", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_category",
+                        column: x => x.categoryid,
+                        principalTable: "category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_publisher",
+                        column: x => x.publisherid,
+                        principalTable: "publisher",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false, defaultValueSql: "nextval('users_id_seq'::regclass)"),
+                    firstname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    lastname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    roleid = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_role",
+                        column: x => x.roleid,
+                        principalTable: "role",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cart",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false, defaultValueSql: "nextval('cart_id_seq'::regclass)"),
+                    userid = table.Column<int>(type: "integer", maxLength: 50, nullable: false),
+                    bookid = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cart", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_book",
+                        column: x => x.bookid,
+                        principalTable: "book",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_user",
+                        column: x => x.userid,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ordermst",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false, defaultValueSql: "nextval('ordermst_id_seq'::regclass)"),
+                    userid = table.Column<int>(type: "integer", nullable: false),
+                    orderdate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    totalprice = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ordermst", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user",
+                        column: x => x.userid,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orderdtl",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false, defaultValueSql: "nextval('orderdtl_id_seq'::regclass)"),
+                    ordermstid = table.Column<int>(type: "integer", nullable: false),
+                    bookid = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    price = table.Column<double>(type: "double precision", nullable: false),
+                    totalprice = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orderdtl", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_orderdtl_book_bookid",
+                        column: x => x.bookid,
+                        principalTable: "book",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_orderdtl_ordermst_ordermstid",
+                        column: x => x.ordermstid,
+                        principalTable: "ordermst",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_book_categoryid",
+                table: "book",
+                column: "categoryid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_book_publisherid",
+                table: "book",
+                column: "publisherid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cart_bookid",
+                table: "cart",
+                column: "bookid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cart_userid",
+                table: "cart",
+                column: "userid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orderdtl_bookid",
+                table: "orderdtl",
+                column: "bookid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orderdtl_ordermstid",
+                table: "orderdtl",
+                column: "ordermstid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ordermst_userid",
+                table: "ordermst",
+                column: "userid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_roleid",
+                table: "user",
+                column: "roleid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cart");
+
+            migrationBuilder.DropTable(
+                name: "orderdtl");
+
+            migrationBuilder.DropTable(
+                name: "book");
+
+            migrationBuilder.DropTable(
+                name: "ordermst");
+
+            migrationBuilder.DropTable(
+                name: "category");
+
+            migrationBuilder.DropTable(
+                name: "publisher");
+
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropTable(
+                name: "role");
+        }
+    }
+}
